acd10b23b40bd9e2f4597fcda991ea5f0a5b9a7e
Anatoly <example@gmail.com>
2020-01-13 02:27:06 +0500

    Добавили условия и цикл

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let money = prompt("Ваш бюджет на месяц?", '');
+let money = +prompt("Ваш бюджет на месяц?", '');
 let time = prompt("Введите дату в формате YYYY-MM-DD", '');
 
 let appData = {
@@ -12,12 +12,29 @@ let appData = {
     saving: false
 }
 
-let a = prompt("Введите обязательную статью расходов в этом месяце");
-let b = prompt("Во сколько обойдется?");
-let c = prompt("Введите обязательную статью расходов в этом месяце");
-let d = prompt("Во сколько обойдется?");
+for(let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце");
+    let b = prompt("Во сколько обойдется?");
 
-appData.expenses[a] = b;
-appData.expenses[c] = d;
+    if ( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null 
+     && a != '' && b != '' && a.length < 50) {
+         console.log("done!");
+        appData.expenses[a] = b;
+    } else {
+        continue;
+    }  
+};
 
-alert(appData.budget / 30);
+appData.moneyPerDay = appData.budget / 30;
+
+alert("Ежедневный бюджет: " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay >= 100 && appData.moneyPerDay <= 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошла ошибка");
+}
